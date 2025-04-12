663fd8cee4ee1d5c17553d101296d70d261496fa
André Luiz Lima <andre.luiz.lima.1997@gmail.com>
2025-04-11 21:03:47 -0300

    submissao

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Digite o numero da conta: ");
+            Integer numero = Integer.parseInt(scanner.nextLine());
+
+            System.out.println("Digite a agencia: ");
+            String agencia = scanner.nextLine();
+
+            System.out.println("Digite o nome: ");
+            String nomeCliente = scanner.nextLine();
+
+            System.out.println("Digite o saldo: ");
+            Double saldo = Double.parseDouble(scanner.nextLine());
+
+            System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");
+        } catch (Exception e) {
+            System.out.println("Ops! Algum erro rolou aí! \n "+e.getMessage());
+        }
+
     }
 }
